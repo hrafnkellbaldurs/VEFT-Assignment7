f326b4e25635f8caf39b43d47bdbb17a58f59601
kotkarl <kotkarl@gmail.com>
2015-10-08 18:09:53 +0000

    A couple of get methods implemented

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 var express = require('express');
 var app = express();
+var companiesRoute = '/api/companies';
+var usersRoute = '/api/users';
 
-app.get('/', (req, res) => {
-	// Set the Content-Type header
-	res.type('text/plain');
-	// Sets the Content-Length header and sends a reply
-	res.send('I\'m a black berry');
+var companies = [ 
+{name: 'Arion Banki', description: 'Bank'},
+{name: 'Bónus', description: 'Supermarket'},
+{name: 'Eimskip', description: 'Shipping'},
+{name: 'AllAround', description: 'Tourist Attractions'}
+];
+
+app.get(companiesRoute, (req, res) => {
+	res.json(companies);
+});
+
+app.get(companiesRoute + '/:id', (req, res) => {
+	var id = req.params.id;
+	if(companies.length <= id || id < 0) {
+		res.statusCode = 404;
+		return res.send('Error 404: No company found');
+	}
+	var q = companies[id];
+	res.json(q);
 });
 
 app.listen(process.env.PORT || 1000);
